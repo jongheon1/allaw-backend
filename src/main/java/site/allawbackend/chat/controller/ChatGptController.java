12c1079a4912100aca47463299a61e2c7ec27de3
jongheon <heon0128@gmail.com>
2024-05-29 21:30:03 +0900

    refactor: api 정리

--- src/main/java/site/allawbackend/chat/controller/ChatGptController.java
+++ src/main/java/site/allawbackend/chat/controller/ChatGptController.java
@@ -2,38 +2,42 @@ package site.allawbackend.chat.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
-import site.allawbackend.chat.service.ChatGptService;
+import site.allawbackend.chat.service.ChatService;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/gpt")
 @RequiredArgsConstructor
 public class ChatGptController {
-    private final ChatGptService chatGptService;
+    private final ChatService chatService;
 
-    @PostMapping("/gen")
-    public String chat(@RequestBody String prompt) {
-        return chatGptService.chat(prompt);
+    @PostMapping("/query")
+    public String query(@RequestBody Map<String, String> request) {
+        String prompt = request.get("prompt");
+        return chatService.chat(prompt);
     }
 
     @PostMapping("/{billId}/summary")
     public String summary(
     @PathVariable Long billId){
-        return chatGptService.summary(billId);
+        return chatService.summary(billId);
     }
 
-    @PostMapping("/{billId}/pdfChat")
-    public String pdfChat(@RequestBody String prompt,
+    @PostMapping("/{billId}/chat")
+    public String chatWithBillContext(@RequestBody Map<String, String> request,
                           @PathVariable Long billId){
-        return chatGptService.checkChat(prompt, billId);
+        String prompt = request.get("prompt");
+        return chatService.chatBasedOnBill(prompt, billId);
     }
 
     @PostMapping("/agree")
-    public String agree(@RequestBody String prompt, String pdf){
-        return chatGptService.agree(prompt, pdf);
+    public String agree(@RequestBody String prompt, String pledge){
+        return chatService.agree(prompt, pledge);
     }
 
     @PostMapping("/disagree")
-    public String disagree(@RequestBody String prompt, String pdf){
-        return chatGptService.disagree(prompt, pdf);
+    public String disagree(@RequestBody String prompt, String pledge){
+        return chatService.disagree(prompt, pledge);
     }
 }
